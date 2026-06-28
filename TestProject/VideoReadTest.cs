6e3b3a69e46e7aedfff66c9c2303bc4ed4faf433
GensokyoFIPA <GensokyoFIPA@163.com>
2026-06-28 14:57:33 +0800

    Add (TestProject.csproj): Add TestProject; Add (VideoReadTest.cs): Aim to test the video load.

---
+++ TestProject/VideoReadTest.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProject
+{
+    internal class VideoReadTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
