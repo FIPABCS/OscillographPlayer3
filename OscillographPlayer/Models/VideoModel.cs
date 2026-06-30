028bd6766d7de8c735bf94664dd367acb02731dc
GensokyoFIPA <GensokyoFIPA@163.com>
2026-06-30 15:26:20 +0800

    Add (VideoModel.cs): Add VideoModel class to store the video data and necessary information.

---
+++ OscillographPlayer/Models/VideoModel.cs
@@ -0,0 +1,45 @@
+﻿using FFMediaToolkit.Decoding;
+using FFMediaToolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace OscillographPlayer.Models
+{
+    public class VideoModel
+    {
+        private readonly MediaFile _video;
+        public MediaFile Video
+        {
+            get => _video;
+        }
+
+        public int Width
+        {
+            get => _video.Video.Info.FrameSize.Width;
+        }
+
+        public int Height
+        {
+            get => _video.Video.Info.FrameSize.Height;
+        }
+
+        public double DurationInMillisecond
+        {
+            get => _video.Video.Info.Duration.TotalMilliseconds;
+        }
+
+        public int TotalFrameNumber
+        {
+            get => _video.Video.Info.NumberOfFrames ?? -1;
+        }
+
+        public double FrameRate
+        {
+            get => _video.Video.Info.AvgFrameRate;
+        }
+
+        public VideoModel(MediaFile video) => _video = video;
+    }
+}
