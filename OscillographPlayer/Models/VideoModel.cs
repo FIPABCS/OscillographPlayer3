8abea6f548335aaa514f7a5a14739e005238679f
GensokyoFIPA <GensokyoFIPA@163.com>
2026-06-30 16:11:01 +0800

    Replace (VideoModel.cs): Add reserved field for each proporty.

--- OscillographPlayer/Models/VideoModel.cs
+++ OscillographPlayer/Models/VideoModel.cs
@@ -15,31 +15,45 @@ namespace OscillographPlayer.Models
             get => _video;
         }
 
+        private readonly int _width;
         public int Width
         {
-            get => _video.Video.Info.FrameSize.Width;
+            get => _width;
         }
 
+        private readonly int _height;
         public int Height
         {
-            get => _video.Video.Info.FrameSize.Height;
+            get => _height;
         }
 
+        private readonly double _durationInMillisecond;
         public double DurationInMillisecond
         {
-            get => _video.Video.Info.Duration.TotalMilliseconds;
+            get => _durationInMillisecond;
         }
 
+        private readonly int _totalFrameNumber;
         public int TotalFrameNumber
         {
-            get => _video.Video.Info.NumberOfFrames ?? -1;
+            get => _totalFrameNumber;
         }
 
+        private readonly double _frameRate;
         public double FrameRate
         {
-            get => _video.Video.Info.AvgFrameRate;
+            get => _frameRate;
         }
 
-        public VideoModel(MediaFile video) => _video = video;
+        public VideoModel(MediaFile video)
+        {
+            _video = video;
+
+            _width = _video.Video.Info.FrameSize.Width;
+            _height = _video.Video.Info.FrameSize.Height;
+            _durationInMillisecond = _video.Video.Info.Duration.TotalMilliseconds;
+            _totalFrameNumber = _video.Video.Info.NumberOfFrames ?? -1;
+            _frameRate = _video.Video.Info.AvgFrameRate;
+        }
     }
 }
