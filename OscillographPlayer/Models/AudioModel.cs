ed299b8181ef5c83d8494f58ffa34715a6f0391d
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-01 19:29:16 +0800

    Add (AudioModel.cs): Add the readonly property AudioFileData of AudioModel class to get the byte array of audio file data.

--- OscillographPlayer/Models/AudioModel.cs
+++ OscillographPlayer/Models/AudioModel.cs
@@ -1,37 +1,38 @@
 ﻿using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace OscillographPlayer.Models
 {
     public class AudioModel
     {
-        private const string chunkID = "RIFF";
+        private const string _chunkID = "RIFF";
 
-        private readonly int chunkSize;
+        private readonly int _chunkSize;
 
-        private const string format = "WAVE";
+        private const string _format = "WAVE";
 
-        private const string subchunk1ID = "fmt ";
+        private const string _subchunk1ID = "fmt ";
 
-        private const short subchunk1Size = 16;
+        private const short _subchunk1Size = 16;
 
-        private const short audioFormat = 1;
+        private const short _audioFormat = 1;
 
-        private const short numChannels = 2;
+        private const short _numChannels = 2;
 
         private readonly int _sampleRate;
 
         private readonly int _byteRate;
 
-        private const short _blockAlign = numChannels * bitsPerSample / 8;
+        private const short _blockAlign = _numChannels * _bitsPerSample / 8;
 
-        private const short bitsPerSample = 16;
+        private const short _bitsPerSample = 16;
 
-        private const string subchunk2ID = "data";
+        private const string _subchunk2ID = "data";
 
-        private readonly int subchunk2Size;
+        private readonly int _subchunk2Size;
 
         private readonly short[] _audioData;
 
@@ -40,12 +41,44 @@ namespace OscillographPlayer.Models
             _audioData = audioData;
 
             int dataSize = _audioData.Length * sizeof(short);
-            subchunk2Size = dataSize;
-            chunkSize = dataSize + 36;
+            _subchunk2Size = dataSize;
+            _chunkSize = dataSize + 36;
 
             _sampleRate = sampleRate;
             
-            _byteRate = _sampleRate * numChannels * bitsPerSample / 8;
+            _byteRate = _sampleRate * _numChannels * _bitsPerSample / 8;
+        }
+
+        public byte[] AudioFileData
+        {
+            get
+            {
+                using var memory = new MemoryStream(_chunkSize + 8);
+                using var writer = new BinaryWriter(memory,Encoding.ASCII,true);
+
+                writer.Write(Encoding.ASCII.GetBytes(_chunkID));
+                writer.Write(_chunkSize);
+                writer.Write(Encoding.ASCII.GetBytes(_format));
+                
+                writer.Write(Encoding.ASCII.GetBytes(_subchunk1ID));
+                writer.Write(_subchunk1Size);
+                writer.Write(_audioFormat);
+                writer.Write(_numChannels);
+                writer.Write(_sampleRate);
+                writer.Write(_byteRate);
+                writer.Write(_blockAlign);
+                writer.Write(_bitsPerSample);
+                
+                writer.Write(Encoding.ASCII.GetBytes(_subchunk2ID);
+                writer.Write(_subchunk2Size);
+
+                foreach (var sampleData in _audioData)
+                {
+                    writer.Write(sampleData); ;
+                }
+
+                return memory.ToArray();
+            }
         }
     }
 }
