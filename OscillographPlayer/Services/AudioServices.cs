59a3cd2a5f9c0e2dbee60adbde36439dadbe5933
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-01 22:48:35 +0800

    Add (AudioServices.cs): Add SaveAudioFileAsync method for AudioServices to save audio file.

---
+++ OscillographPlayer/Services/AudioServices.cs
@@ -0,0 +1,31 @@
+﻿using OscillographPlayer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscillographPlayer.Services
+{
+    public static class AudioServices
+    {
+        public static async Task SaveAudioFileAsync(AudioModel audioFileData,string fileName,string filePath)
+        {
+            if(string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            if(!Path.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            await File.WriteAllBytesAsync(Path.Combine(filePath, fileName), audioFileData.AudioFileData);
+        }
+    }
+}
