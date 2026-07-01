4ca53868b07744ebb2d794ce05d7e31f36193cbb
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-01 15:53:46 +0800

    Add (VideoServices.cs): VideoServices class include the method to open video file and check if video stream exist.

---
+++ OscillographPlayer/Services/VideoServices.cs
@@ -0,0 +1,41 @@
+﻿using FFMediaToolkit.Decoding;
+using FFMediaToolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OscillographPlayer.Services
+{
+    public static class VideoServices
+    {
+        public static MediaFile OpenAndDecodeVideo(ref string path)
+        {
+            MediaFile video;
+            
+            try
+            {
+                MediaOptions options = new() { VideoPixelFormat = ImagePixelFormat.Gray8 };
+
+                video = MediaFile.Open(path, options);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Fail Open Video File: {ex.Message}");
+            }
+
+            return video;
+        }
+
+        public static bool VideoStreamExist(ref MediaFile video)
+        {
+            if(video!= null&&video.Video.Info.NumberOfFrames!=0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
