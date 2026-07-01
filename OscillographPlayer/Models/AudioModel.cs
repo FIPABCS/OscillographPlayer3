9ffdde66c44033d5ebe58cf79593b604a5ec3bd0
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-01 17:29:24 +0800

    Add (AudioModel.cs): Add AudioModel class to store the audio data and wave file data, no getter now.

---
+++ OscillographPlayer/Models/AudioModel.cs
@@ -0,0 +1,51 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OscillographPlayer.Models
+{
+    public class AudioModel
+    {
+        private const string chunkID = "RIFF";
+
+        private readonly int chunkSize;
+
+        private const string format = "WAVE";
+
+        private const string subchunk1ID = "fmt ";
+
+        private const short subchunk1Size = 16;
+
+        private const short audioFormat = 1;
+
+        private const short numChannels = 2;
+
+        private readonly int _sampleRate;
+
+        private readonly int _byteRate;
+
+        private const short _blockAlign = numChannels * bitsPerSample / 8;
+
+        private const short bitsPerSample = 16;
+
+        private const string subchunk2ID = "data";
+
+        private readonly int subchunk2Size;
+
+        private readonly short[] _audioData;
+
+        public AudioModel(short[] audioData,int sampleRate)
+        {
+            _audioData = audioData;
+
+            int dataSize = _audioData.Length * sizeof(short);
+            subchunk2Size = dataSize;
+            chunkSize = dataSize + 36;
+
+            _sampleRate = sampleRate;
+            
+            _byteRate = _sampleRate * numChannels * bitsPerSample / 8;
+        }
+    }
+}
