069bfbead4d0ac64fe983476037a7f0e6292e74a
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-16 00:41:10 +0800

    Add (WaveGenerate): Add the inovke of unmanaged method EdgeDetection in WaveGenerateCore.

--- OscillographPlayer.WaveGenerateLib/WaveGenerate.cs
+++ OscillographPlayer.WaveGenerateLib/WaveGenerate.cs
@@ -1,7 +1,25 @@
-﻿namespace OscillographPlayer.WaveGenerateLib
-{
-    public class WaveGenerate
-    {
+﻿using System;
+using System.Runtime.InteropServices;
 
+namespace OscillographPlayer.WaveGenerateLib
+{
+    public partial class WaveGenerate
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct NoiseReductionCore
+        {
+            private IntPtr weight;
+            private int radius;
+        };
+
+        [LibraryImport(@"OscillographPlayer.WaveGenerateCore.dll", EntryPoint = "EdgeDetection")]
+        private static partial void EdgeDetectionUnsafe(
+            [In, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U1)]
+            byte[] grayImage, 
+            int width, int height,
+            float skip, byte lowThreshold, byte highThreshold,
+            [Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U1)]
+            byte[] edgeMap
+        );
     }
 }
