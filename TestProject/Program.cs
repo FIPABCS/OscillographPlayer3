8b510cabdde6015b03d2503d679347624fc1469c
GensokyoFIPA <GensokyoFIPA@163.com>
2026-07-29 23:23:25 +0800

    Add (AmplitudeMaximization): Add the limit logic for the multiple; Replace (WaveGenerateLib): Fix the marshal method of managed data.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,7 @@ namespace TestProject
             //VideoLoadTest.LoadAndPrintVideoFrames();
             //WaveGenerateTest.EdgeDetectionAndPrintFrame();
 
-            WaveGenerateTest.WaveGenerateAndPrintPoints(2.0f, 32, 128, WaveGenerateServices.ArrangeMethod.ByPoint, 30.0, 28800, false, false, false);
+            WaveGenerateTest.WaveGenerateAndPrintPoints(2.0f, 32, 128, WaveGenerateServices.ArrangeMethod.ByPoint, 30.0, 28800, false, true, true);
 
             return;
         }
