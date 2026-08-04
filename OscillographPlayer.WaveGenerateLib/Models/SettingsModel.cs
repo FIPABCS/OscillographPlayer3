7694677cd7ca6fe9d5de324d9bbafb5d1006a788
GensokyoFIPA <GensokyoFIPA@163.com>
2026-08-05 00:21:09 +0800

    Add (GenerateFromVideoView): Add controls for page GenerateFromVideoView.

--- OscillographPlayer.WaveGenerateLib/Models/SettingsModel.cs
+++ OscillographPlayer.WaveGenerateLib/Models/SettingsModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OscillographPlayer.Models
+namespace OscillographPlayer.WaveGenerateLib.Models
 {
     public class SettingsModel
     {
