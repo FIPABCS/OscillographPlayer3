d2cf83a23f170c459ed0e5a8993307043b8d1533
GensokyoFIPA <GensokyoFIPA@163.com>
2026-08-04 17:02:17 +0800

    Add (GenerateFormVideoPage): Add the view GenerateFromVideo.

---
+++ OscillographPlayer/Views/GenerateFromVideoPage.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OscillographPlayer.Views
+{
+    public partial class GenerateFromVideoPage : UserControl
+    {
+        public GenerateFromVideoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
