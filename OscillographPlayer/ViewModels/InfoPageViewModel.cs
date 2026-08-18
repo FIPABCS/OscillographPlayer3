22a13de3084794ba2edd387812439a49861e0dff
GensokyoFIPA <GensokyoFIPA@163.com>
2026-08-19 01:39:17 +0800

    Add (InfoPage): Add the multilingual support for InfoPage.

--- OscillographPlayer/ViewModels/InfoPageViewModel.cs
+++ OscillographPlayer/ViewModels/InfoPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OscillographPlayer.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,27 @@ namespace OscillographPlayer.ViewModels
 {
     public partial class InfoPageViewModel:ViewModelBase
     {
+
+        //View Text
+        public static IObservable<string?> Title => LanguageHelper.Instance.InfoPage_Title;
+
+        public static IObservable<string?> CurrentVersionCardTitle => LanguageHelper.Instance.InfoPage_CurrentVersionCardTitle;
+        public static IObservable<string?> ViewRelease => LanguageHelper.Instance.InfoPage_ViewRelease;
+
+        public static IObservable<string?> RepositoryCardTitle => LanguageHelper.Instance.InfoPage_RepositoryCardTitle;
+        public static IObservable<string?> RepositoryCardDes => LanguageHelper.Instance.InfoPage_RepositoryCardDes;
+
+        public static IObservable<string?> LicenseCardTitle => LanguageHelper.Instance.InfoPage_LicenseCardTitle;
+        public static IObservable<string?> LicenseCardDes => LanguageHelper.Instance.InfoPage_LicenseCardDes;
+
+        public static IObservable<string?> HelpAndFeedbackSubtitle => LanguageHelper.Instance.InfoPage_HelpAndFeedbackSubtitle;
+
+        public static IObservable<string?> HelpCardTitle => LanguageHelper.Instance.InfoPage_HelpCardTitle;
+        public static IObservable<string?> HelpCardDes => LanguageHelper.Instance.InfoPage_HelpCardDes;
+
+        public static IObservable<string?> FeedbackCardTitle => LanguageHelper.Instance.InfoPage_FeedbackCardTitle;
+        public static IObservable<string?> FeedBackCardDes => LanguageHelper.Instance.InfoPage_FeedbackCardDes;
+
+        public static IObservable<string?> DependencyAndReferenceSubtitle => LanguageHelper.Instance.InfoPage_DependencyAndReferenceSubtitle;
     }
 }
