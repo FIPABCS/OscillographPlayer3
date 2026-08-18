241cf08deae8de6bcc9975c48b1b75bf0e171749
GensokyoFIPA <GensokyoFIPA@163.com>
2026-08-19 00:01:04 +0800

    Add (SettingsPage): Add the method to change theme.

--- OscillographPlayer/ViewModels/SettingsPageViewModel.cs
+++ OscillographPlayer/ViewModels/SettingsPageViewModel.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 using Irihi.Lingua;
+using Avalonia.Styling;
+using System.Drawing;
+using AvaloniaFluentUI.Styling;
 
 namespace OscillographPlayer.ViewModels
 {
@@ -12,14 +15,14 @@ namespace OscillographPlayer.ViewModels
     {
         //Language Option
         [ObservableProperty]
-        private List<LinguaCulture> _languageList = new List<LinguaCulture>()
-        {
+        public partial List<LinguaCulture> LanguageList { get; set; } =
+        [
             new(){Culture=new CultureInfo("zh-CN"),DisplayName="中文 (简体, 中国大陆)"},
             new(){Culture=new CultureInfo("en-US"),DisplayName="English (United States)"}
-        };
+        ];
 
         [ObservableProperty]
-        private LinguaCulture _selectedLanguage;
+        public partial LinguaCulture SelectedLanguage { get; set; }
 
         partial void OnSelectedLanguageChanged(LinguaCulture value)
         {
@@ -35,24 +38,44 @@ namespace OscillographPlayer.ViewModels
         }
 
         //Theme Option
+        public record class ThemeOption 
+        {
+            public required ThemeVariant Theme { get; init; }
+            public required IObservable<string?> Description { get; init; }
+        }
 
+        [ObservableProperty]
+        public partial List<ThemeOption> ThemeOptions { get; set; } =
+        [
+            new () {Theme=ThemeVariant.Default,Description=LanguageHelper.Instance.SettingsPage_ThemeOption_System},
+            new () {Theme=ThemeVariant.Light,Description=LanguageHelper.Instance.SettingsPage_ThemeOption_Light},
+            new () {Theme=ThemeVariant.Dark,Description=LanguageHelper.Instance.SettingsPage_ThemeOption_Dark},
+        ];
+        [ObservableProperty]
+        public partial ThemeOption SelectedThemeOption { get; set; }
+
+        partial void OnSelectedThemeOptionChanged(ThemeOption value)
+        {
+            AvaloniaFluentTheme.Instance.CurrentTheme = value.Theme;
+        }
 
         //View Text
-        public IObservable<string?> PageTitle=>LanguageHelper.Instance.SettingsPage_Title;
+        public static IObservable<string?> PageTitle=>LanguageHelper.Instance.SettingsPage_Title;
+
+        public static IObservable<string?> LanguageSubtitle=>LanguageHelper.Instance.SettingsPage_LanguageSubtitle;
+
+        public static IObservable<string?> LanguageCardTitle=>LanguageHelper.Instance.SettingsPage_LanguageCardTitle;
+        public static IObservable<string?> LanguageCardDes => LanguageHelper.Instance.SettingsPage_LanguageCardDes;
         
-        public IObservable<string?> LanguageSubtitle=>LanguageHelper.Instance.SettingsPage_LanguageSubtitle;
-        
-        public IObservable<string?> LanguageCardTitle=>LanguageHelper.Instance.SettingsPage_LanguageCardTitle;
-        public IObservable<string?> LanguageCardDes => LanguageHelper.Instance.SettingsPage_LanguageCardDes;
-        
-        public IObservable<string?> AppearanceSubtitlt=>LanguageHelper.Instance.SettingsPage_AppearanceSubtitle;
-        
-        public IObservable<string?> ThemeCardTitle=>LanguageHelper.Instance.SettingsPage_ThemeCardTitle;
-        public IObservable<string?> ThemeCardDes => LanguageHelper.Instance.SettingsPage_ThemeCardDes;
+        public static IObservable<string?> AppearanceSubtitlt=>LanguageHelper.Instance.SettingsPage_AppearanceSubtitle;
+
+        public static IObservable<string?> ThemeCardTitle=>LanguageHelper.Instance.SettingsPage_ThemeCardTitle;
+        public static IObservable<string?> ThemeCardDes => LanguageHelper.Instance.SettingsPage_ThemeCardDes;
 
         public SettingsPageViewModel()
         {
             SelectedLanguage = LanguageList[0];
+            SelectedThemeOption = ThemeOptions[0];
         }
     }
 
